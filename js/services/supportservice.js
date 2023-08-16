2a1da91acabda0b325baf95bd123e53f17f8a6ef
SecondLuke242014 <136903118+SecondLuke242014@users.noreply.github.com>
2023-08-16 18:01:55 +0400

    Update supportservice.js

--- js/services/supportservice.js
+++ js/services/supportservice.js
@@ -3,6 +3,12 @@ var email = document.getElementByID('email')
 
 button.addEventListener('click', function(){
   email.send({
-  
+    Host: 'mail.google.com',
+    Username: 'support@secondluke242014.github.io',
+    Password: 'bot123',
+    To: email.value,
+    From: 'support@secondluke242014.github.io',
+    Subject: 'SecondLuke242014 Support',
+    Body: ''
   })
 })
