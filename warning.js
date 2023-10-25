0d07c5775b6cd5fa8ad767bc38e4a30f61ed6495
LukeOfficial <136903118+SecondLuke242014@users.noreply.github.com>
2023-10-25 17:02:19 +0400

    Create warning.js

---
+++ warning.js
@@ -0,0 +1,2 @@
+console.log('%cSTOP!','font-size:55px;color:red;font-weight:bold')
+console.log("This is your browser's developing tools. And please do not modify these on the website.\nIf someone tells you to copy and paste code, please DO NOT do it.\nAnd if you are sure, you will have suspension.\nIf you don't care, please leave this website with warnings.")
