4af4de165f010211db241515e85398ba371b1bda
LukeOfficial <e87624273@gmail.com>
2024-01-31 13:44:47 +0400

    Update ads.js

--- js/ads.js
+++ js/ads.js
@@ -2,4 +2,7 @@ var adImage = document.getElementById("adImage")
 var assetsadspath = "assets/ads/"
 var ads = [
   "ad1.png",
+  "ad2.png",
 ]
+
+adImage.src = ads[Math.floor(Math.random() * ads.length)]
